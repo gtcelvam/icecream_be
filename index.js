0a30ccd0b237ce53e9c0273eba2fb13734ed7d7e
Gtcelvam <selvasizzler@gmail.com>
2022-03-14 11:29:29 +0530

    ENV File Added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const skill = require('./model/skill');
 const mongoose = require('mongoose');
+const dotEnv = require('dotenv').config();
 
 
 app.use(express.json());
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(skill)
 
 //DB Connection
-mongoose.connect('mongodb+srv://thselvan:gtcelvaM2005@ts-admin.0c5cq.mongodb.net/ts_admin?retryWrites=true&w=majority',()=>{
+mongoose.connect(process.env.MONGODB,()=>{
     console.log("DB Connected Successfully!");
 })
 
