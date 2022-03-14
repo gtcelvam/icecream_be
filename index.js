795242d5634b25053fb3064d2bde13698685b9df
Gtcelvam <selvasizzler@gmail.com>
2022-03-14 12:07:26 +0530

    MongoDB Con 3

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb+srv://thselvan:gtcelvaM2005@ts-admin.0c5cq.mongodb.net
     console.log("DB Connected Successfully!");
 })
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 app.listen(port,()=>{
     console.log("Server Started Successfully!");
 })
